bfde9421530dead1b03f7b563e849c8749da768f
ChaitanyDalvi06 <2023.chaitanyad@isu.ac.in>
2024-06-12 13:21:21 +0530

    Your commit message here

---
+++ redux-demo/src/App.jsx
@@ -0,0 +1,28 @@
+import './App.css'
+import Home from './pages/home/Home'
+import About from './pages/about/About'
+import Services from './services/Services'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+
+
+function App() {
+  
+
+  return (
+    <>
+
+      <BrowserRouter>
+      <Routes>
+        <Route path='' element={<Home/>}/>
+        <Route path='/home' element={<Home/>}/>
+        <Route path='/about' element={<About/>}/>
+        <Route path='/services' element={<Services/>}/>
+      </Routes>
+      </BrowserRouter>
+      
+
+    </>
+  )
+}
+
+export default App
